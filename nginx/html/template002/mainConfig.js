2cecf548077efde30b38a749b54eef51e959945c
Andrey N. Burdin <burdinan@it-sakh.net>
2022-02-15 01:08:13 +1100

    Обновлена версия и удалены неиспользуемые переменные

--- nginx/html/template002/mainConfig.js
+++ nginx/html/template002/mainConfig.js
@@ -9,7 +9,7 @@
  * @copyright Бурдин А.Н. <support@it-sakh.net>
  * @link      http://www.it-sakh.info/SportInfo/
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
- * @version   1.0.4
+ * @version   1.0.5
  */
 
 //Общие настройки
@@ -17,9 +17,3 @@
 // Значение: true  - Включено
 // Значение: false - Выключено
 var debuging = true;
-
-//На сколько секунд показываем титры: Название соревнования
-let AutoCloseTVSegment = 10;
-
-// Заголовок для кнопки: 
-var TitleSubNameJudgeAll = "";
